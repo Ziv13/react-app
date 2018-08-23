d9d206245f03cbadf6a3691f3e6e9acc48884c05
Ziv13 <1223550806@qq.com>
2018-08-23 22:20:45 +0800

    8-23 update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
-
+import { withRouter, Link } from  'react-router-dom'
+import connect from './modules/connect'
 
 class App extends Component {
+
+  componentWillReceiveProps (props){
+    //当路由切换的时候
+    let {pathname} = props.location
+    if ( pathname !== this.props.location.pathname ){
+        this.checkLogin(props)
+        
+    }
+  }
+
+  componentWillMount () {
+    //当直接进入某个路由的时候判断是否登录
+    this.checkLogin(this.props)
+  }
+
+  checkLogin (props) {//登录判断函数
+    let {commons, history} = this.props 
+    if (props.location.pathname !== '/login' ){
+      if ( !commons.user_state ){
+        history.replace('/login')
+      }
+    }
+  }
+
+
+
   render() {
     return (
       <div className="App">
+        <Link to = "/login">登录</Link>
         {this.props.children}
       </div>
     );
   }
 }
 
-export default App
+export default connect(withRouter(App), 'commons')
